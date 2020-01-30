171e82189aee0f138e7d0b3fa46479a8d08bf9e2
jorge grijalva <jgrijalva10@outlook.com>
2020-01-30 15:49:05 -0600

    Actualizacion Nosotros

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { NosotrosComponent } from './components/nosotros/nosotros.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: 'nosotros',
+    component: NosotrosComponent
   }
 ];
 
