bfba891d7944367599fe0d50fbe5f5b6af65896c
Brayan <brayan>
2020-01-30 12:04:10 -0600

    marcas y contacto

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent
+  },
+  {
+    path: '',
+    component: ContactoComponent
   }
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,9 +6,11 @@ import { AppComponent } from './app.component';
 import { BodyComponent } from './components/body/body.component';
 import { ConstruccionComponent } from './components/construccion/construccion.component';
 import { HomeComponent } from './components/home/home.component';
+import { MarcasComponent } from './components/marcas/marcas.component';
+import { ContactoComponent } from './components/contacto/contacto.component';
 
 @NgModule({
-  declarations: [AppComponent, BodyComponent, ConstruccionComponent, HomeComponent],
+  declarations: [AppComponent, BodyComponent, ConstruccionComponent, HomeComponent, MarcasComponent, ContactoComponent],
   imports: [BrowserModule, AppRoutingModule],
   providers: [],
   bootstrap: [AppComponent]
