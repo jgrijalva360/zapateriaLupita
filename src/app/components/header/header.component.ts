62431e5613f765c410de4d4e5f52103718eaef9f
jorge grijalva <jgrijalva10@outlook.com>
2020-02-03 14:57:26 -0600

    Actualizacion general

--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -12,27 +12,15 @@ export class HeaderComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    // this.navbar();
+    this.navbar();
   }
-  // navbar() {
-  //   // const mobileDetect = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/.test(
-  //   //   navigator.userAgent
-  //   // );
-  //   $(window).scroll(() => {
-  //     if ($(document).scrollTop() > 80) {
-  //       $('.navbar').addClass('bg-dark');
-  //     } else {
-  //       $('.navbar').removeClass('bg-dark');
-  //     }
-  //   });
-  // }
-  // menu() {
-  //   if ($('.bg-dark').length) {
-  //     if ($(document).scrollTop() < 80) {
-  //       $('.navbar').removeClass('bg-dark');
-  //     }
-  //   } else {
-  //     $('.navbar').addClass('bg-dark');
-  //   }
-  // }
+  navbar() {
+    $(window).scroll(() => {
+      if ($(document).scrollTop() > 80) {
+        $('.navbar').removeClass('d-none');
+      } else {
+        $('.navbar').addClass('d-none');
+      }
+    });
+  }
 }
